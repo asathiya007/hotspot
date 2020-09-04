faebb624efe2c6cfc128389a823fda160a0c98cc
asathiya6 <asathiya6@gatech.edu>
2020-09-04 10:57:51 -0700

    fixing issues in deployment

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div class="area" >
-            <ul class="circles">
+        <div className="area" >
+            <ul className="circles">
                     <li></li>
                     <li></li>
                     <li></li>
